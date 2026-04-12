2111ca3b416021ac1332b68792fdd71dcd98c7e2
Simon Brown <simon@structurizr.com>
2026-04-12 09:25:14 +0100

    .

--- js/ad.js
+++ js/ad.js
@@ -1,46 +1,46 @@
 const ads = [
-    {
-        img: 'patreon-structurizr.png',
-        href: 'https://patreon.com/structurizr',
-        expiry: new Date('2026-12-31'),
-        targets: [ 'structurizr', 'simonbrown' ]
-    },
-    {
-        img: 'patreon-c4model.png',
-        href: 'https://patreon.com/c4model',
-        expiry: new Date('2026-12-31'),
-        targets: [ 'c4model', 'simonbrown' ]
-    },
+    // {
+    //     img: 'patreon-structurizr.png',
+    //     href: 'https://patreon.com/structurizr',
+    //     expiry: new Date('2026-12-31'),
+    //     targets: [ 'structurizr', 'simonbrown' ]
+    // },
+    // {
+    //     img: 'patreon-c4model.png',
+    //     href: 'https://patreon.com/c4model',
+    //     expiry: new Date('2026-12-31'),
+    //     targets: [ 'c4model', 'simonbrown' ]
+    // },
     {
         img: 'book.png',
         href: 'https://www.oreilly.com/library/view/the-c4-model/9798341660113/',
         expiry: new Date('2026-12-31'),
-        targets: [ 'c4model', 'structurizr', 'simonbrown' ]
+        targets: [ 'c4model', 'structurizr' ]
     },
-    {
-        img: 'structurizr.png',
-        href: 'https://docs.structurizr.com',
-        expiry: new Date('2026-12-31'),
-        targets: [ 'c4model', 'simonbrown' ]
-    },
+    // {
+    //     img: 'structurizr.png',
+    //     href: 'https://docs.structurizr.com',
+    //     expiry: new Date('2026-12-31'),
+    //     targets: [ 'c4model', 'simonbrown' ]
+    // },
     {
         img: 'trifork-nl.png',
         href: 'https://trifork.nl/academy/our-courses/software-architecture-for-developers/',
         expiry: new Date('2026-05-11'),
         targets: [ 'structurizr', 'c4model', 'simonbrown' ]
     },
-    {
-        img: 'goto-cph.png',
-        href: 'https://gotocph.com/2026/masterclasses/583/2-days-software-architecture-for-developers',
-        expiry: new Date('2026-09-27'),
-        targets: [ 'structurizr', 'c4model', 'simonbrown' ]
-    },
-    {
-        img: 'sa4d-workshop.png',
-        href: 'https://simonbrown.je/workshops',
-        expiry: new Date('2026-12-31'),
-        targets: [ 'structurizr', 'c4model', 'simonbrown' ]
-    },
+    // {
+    //     img: 'goto-cph.png',
+    //     href: 'https://gotocph.com/2026/masterclasses/583/2-days-software-architecture-for-developers',
+    //     expiry: new Date('2026-09-27'),
+    //     targets: [ 'structurizr', 'c4model', 'simonbrown' ]
+    // },
+    // {
+    //     img: 'sa4d-workshop.png',
+    //     href: 'https://simonbrown.je/workshops',
+    //     expiry: new Date('2026-12-31'),
+    //     targets: [ 'structurizr', 'c4model', 'simonbrown' ]
+    // },
     {
         img: 'c4-workshop.png',
         href: 'https://simonbrown.je/workshops',
